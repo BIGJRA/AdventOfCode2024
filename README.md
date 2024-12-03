8252f426781a1817c3cd04bd7743d166ad3c549e
BIGJRA <47573834+BIGJRA@users.noreply.github.com>
2024-12-03 14:10:35 -0500

    day3

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Welcome to my Advent of Code 2024 solutions repository! This year I decided to p
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -78,16 +78,16 @@ Both parts: 1.322ms
 
 ```
 Day 02
-Time part 1: 1.038ms
-Time part 2: 2.419ms
-Both parts: 3.457ms
+Time part 1: 0.9ms
+Time part 2: 2.487ms
+Both parts: 3.387ms
 ```
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.244ms
+Time part 2: 0.174ms
+Both parts: 0.418ms
 ```
 
 ```
@@ -245,8 +245,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 4.779ms
+Total stars: 6/50
+Total time: 5.127ms
 ```
 
 <!--/RESULTS-->
--- src/day03/index.js
+++ src/day03/index.js
@@ -4,32 +4,54 @@ const parseInput = (rawInput) => rawInput;
 
 const part1 = (rawInput) => {
   const input = parseInput(rawInput);
-
-  return;
+  const matches = [...input.matchAll(/mul\(\d+,\d+\)/g)];
+  // Regex matchAll function returns an array of RegExp match arrays: the matched text is in the first element of each
+  let res = 0;
+  matches.forEach((m) => {
+    let nums = m[0].slice(4).slice(0, -1).split(",");
+    res += nums[0] * nums[1];
+  });
+  return res;
 };
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput);
-
-  return;
+  const matches = [...input.matchAll(/(mul\(\d+,\d+\))|(do\(\))|(don't\(\))/g)];
+  let res = 0;
+  let enabled = true;
+  matches.forEach((m) => {
+    if (m[0] == "do()") {
+      enabled = true;
+    } else if (m[0] == "don't()") {
+      enabled = false;
+    } else if (enabled) {
+      let nums = m[0].slice(4).slice(0, -1).split(",");
+      res += nums[0] * nums[1];
+    }
+  });
+  return res;
 };
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+        `,
+        expected: 161,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))        
+        `,
+        expected: 48,
+      },
     ],
     solution: part2,
   },
